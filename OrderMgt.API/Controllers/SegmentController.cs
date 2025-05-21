e33c81c6929bf2e561a93999c567c0441859cc30
mumba mumba <kayebele@yahoo.com>
2025-05-21 21:02:09 +0200

    Added segment controller

---
+++ OrderMgt.API/Controllers/SegmentController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderMgt.API.Interfaces;
+using OrderMgt.API.Repositories;
+using OrderMgt.Model.Entities;
+using OrderMgt.Model.Models;
+using Serialize.Linq.Serializers;
+using System.Linq.Expressions;
+
+namespace OrderMgt.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SegmentController : ControllerBase
+    {
+        protected readonly ISegmentRepository repository;
+        protected readonly ILogger<SegmentController> logger;
+
+        public SegmentController(SegmentRepository _segmentRepository, ILogger<SegmentController> _logger)
+        {
+            repository = _segmentRepository;
+            logger = _logger;
+        }
+
+        [HttpPost("AddAsync")]
+        public async Task<IActionResult> AddAsync(Segment segment)
+        {
+            if (await repository.AddAsync(segment))
+            {
+                return Ok(new BaseResponseModel { Success = true, Data = segment });
+            }
+            else
+            {
+                return NotFound(new BaseResponseModel { Success = false, ErrorMessage = "" });
+            }
+        }
+
+        [HttpPost("AddRangeAsync")]
+        public async Task<IActionResult> AddRangeAsync(IEnumerable<Segment> segments)
+        {
+            if (await repository.AddRangeAsync(segments))
+            {
+                return Ok(new BaseResponseModel { Success = true, Data = segments });
+            }
+            else
+            {
+                return NotFound(new BaseResponseModel { Success = false, ErrorMessage = "" });
+            }
+        }
+
+        [HttpGet("Find")]
+        public IActionResult Find(string predicate)
+        {
+            var serializer = new ExpressionSerializer(new Serialize.Linq.Serializers.JsonSerializer());
+            string serializedExpression = predicate.Replace('@', '#');
+
+            Expression<Func<Segment, bool>> result = (Expression<Func<Segment, bool>>)serializer.DeserializeText(serializedExpression);
+
+            var res = repository.Find(result);
+            if (res.Count() > 0)
+            {
+                return Ok(new BaseResponseModel { Success = true, Data = res });
+            }
+            else
+            {
+                return Ok(new BaseResponseModel { Success = false, Data = null });
+            }
+        }
+
+        [HttpGet("GetAsync")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var res = await repository.GetAsync(id);
+            return Ok(new BaseResponseModel { Success = true, Data = res });
+        }
+
+        [HttpGet("GetAllAsync")]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var res = await repository.GetAllAsync();
+            return Ok(new BaseResponseModel { Success = true, Data = res });
+        }
+
+        [HttpDelete("RemoveAsync")]
+        public async Task<IActionResult> RemoveAsync(Segment segment)
+        {
+            if (await repository.RemoveAsync(segment))
+            {
+                return Ok(new BaseResponseModel { Success = true });
+            }
+            else
+            {
+                return NotFound(new BaseResponseModel { Success = false });
+            }
+
+        }
+
+        [HttpDelete("RemoveRangeAsync")]
+        public async Task<IActionResult> RemoveRangeAsync(IEnumerable<Segment> segments)
+        {
+            if (await repository.RemoveRangeAsync(segments))
+            {
+                return Ok(new BaseResponseModel { Success = true });
+            }
+            else
+            {
+                return NotFound(new BaseResponseModel { Success = false });
+            }
+        }
+
+        [HttpPut("UpdateAsync")]
+        public async Task<IActionResult> UpdateAsync(Segment segment)
+        {
+            if (await repository.UpdateAsync(segment))
+            {
+                return Ok(new BaseResponseModel { Success = true, Data = segment });
+            }
+            else
+            {
+                return NotFound(new BaseResponseModel { Success = false });
+            }
+        }
+
+        [HttpPut("UpdateRangeAsync")]
+        public async Task<IActionResult> UpdateRangeAsync(IEnumerable<Segment> segments)
+        {
+            if (await repository.UpdateRangeAsync(segments))
+            {
+                return Ok(new BaseResponseModel { Success = true, Data = segments });
+            }
+            else
+            {
+                return NotFound(new BaseResponseModel { Success = false });
+            }
+        }
+    }
+}
