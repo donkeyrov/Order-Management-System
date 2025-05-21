e6309652664604cccc224b2ec246e01787f9b7be
mumba mumba <kayebele@yahoo.com>
2025-05-21 21:45:28 +0200

    Added transaction controller

---
+++ OrderMgt.API/Controllers/TransactionController.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderMgt.API.Interfaces;
+using OrderMgt.API.Repositories;
+using OrderMgt.Model.Entities;
+using OrderMgt.Model.Models;
+using Serialize.Linq.Serializers;
+using System.Linq.Expressions;
+
+namespace OrderMgt.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        protected readonly ITransactionRepository repository;
+        protected readonly ILogger<TransactionController> logger;
+        public TransactionController(TransactionRepository _transactionRepository, ILogger<TransactionController> _logger)
+        {
+            repository = _transactionRepository;
+            logger = _logger;
+        }
+
+        [HttpPost("AddAsync")]
+        public async Task<IActionResult> AddAsync(Transaction transaction)
+        {
+            if (await repository.AddAsync(transaction))
+            {
+                return Ok(new BaseResponseModel { Success = true, Data = transaction });
+            }
+            else
+            {
+                return NotFound(new BaseResponseModel { Success = false, ErrorMessage = "" });
+            }
+        }
+
+        [HttpPost("AddRangeAsync")]
+        public async Task<IActionResult> AddRangeAsync(IEnumerable<Transaction> transactions)
+        {
+            if (await repository.AddRangeAsync(transactions))
+            {
+                return Ok(new BaseResponseModel { Success = true, Data = transactions });
+            }
+            else
+            {
+                return NotFound(new BaseResponseModel { Success = false, ErrorMessage = "" });
+            }
+        }
+
+        [HttpGet("Find")]
+        public IActionResult Find(string predicate)
+        {
+            var serializer = new ExpressionSerializer(new Serialize.Linq.Serializers.JsonSerializer());
+            string serializedExpression = predicate.Replace('@', '#');
+
+            Expression<Func<Transaction, bool>> result = (Expression<Func<Transaction, bool>>)serializer.DeserializeText(serializedExpression);
+
+            var res = repository.Find(result);
+            if (res.Count() > 0)
+            {
+                return Ok(new BaseResponseModel { Success = true, Data = res });
+            }
+            else
+            {
+                return Ok(new BaseResponseModel { Success = false, Data = null });
+            }
+        }
+
+        [HttpGet("GetAsync")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var res = await repository.GetAsync(id);
+            return Ok(new BaseResponseModel { Success = true, Data = res });
+        }
+
+        [HttpGet("GetAllAsync")]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var res = await repository.GetAllAsync();
+            return Ok(new BaseResponseModel { Success = true, Data = res });
+        }
+
+        [HttpDelete("RemoveAsync")]
+        public async Task<IActionResult> RemoveAsync(Transaction transaction)
+        {
+            if (await repository.RemoveAsync(transaction))
+            {
+                return Ok(new BaseResponseModel { Success = true });
+            }
+            else
+            {
+                return NotFound(new BaseResponseModel { Success = false });
+            }
+
+        }
+
+        [HttpDelete("RemoveRangeAsync")]
+        public async Task<IActionResult> RemoveRangeAsync(IEnumerable<Transaction> transactions)
+        {
+            if (await repository.RemoveRangeAsync(transactions))
+            {
+                return Ok(new BaseResponseModel { Success = true });
+            }
+            else
+            {
+                return NotFound(new BaseResponseModel { Success = false });
+            }
+        }
+
+        [HttpPut("UpdateAsync")]
+        public async Task<IActionResult> UpdateAsync(Transaction transaction)
+        {
+            if (await repository.UpdateAsync(transaction))
+            {
+                return Ok(new BaseResponseModel { Success = true, Data = transaction });
+            }
+            else
+            {
+                return NotFound(new BaseResponseModel { Success = false });
+            }
+        }
+
+        [HttpPut("UpdateRangeAsync")]
+        public async Task<IActionResult> UpdateRangeAsync(IEnumerable<Transaction> transactions)
+        {
+            if (await repository.UpdateRangeAsync(transactions))
+            {
+                return Ok(new BaseResponseModel { Success = true, Data = transactions });
+            }
+            else
+            {
+                return NotFound(new BaseResponseModel { Success = false });
+            }
+        }
+    }
+}
